b8990157729612eb5e64b6ecb4944496310347ab
Patryk Jarzynka <patryk.jarzynka@codeconcept.pl>
2025-07-23 13:19:59 +0200

    Implemented home page. Installed path library to handle importing files with usage of @ symbol.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
+<script setup lang="ts">
+  import Home from '@/components/view/Home.vue'
+</script>
+
 <template>
-  <div
-    class="flex justify-center flex-col gap-4 items-center"
-  >
-    <div>
-      <a href="https://nubisoft.io/" target="_blank">
-        <img src="./assets/nubisoft.svg" class="" alt="Nubisoft logo" />
-      </a>
-    </div>
-    <h1>NubiWeather</h1>
-    <section>
-      <h2>Here's your starting point.</h2>
-      <h3>Good luck! :)</h3>
-    </section>
-  </div>
+  <Home/>
 </template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/view/Home.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div
+    class="flex flex-col ga-10 items-center"
+  >
+    <div class="d-flex flex-column align-center ga-3">
+      <div>
+        <a href="https://nubisoft.io/" target="_blank">
+          <img src="@/assets/nubisoft.svg" class="" alt="Nubisoft logo" />
+        </a>
+      </div>
+
+
+      <h1>Welcome to <span class="highlight-text">NubiWeather</span> </h1>
+    </div>
+
+
+    <v-btn
+      size="large"
+      variant="outlined"
+    >
+      Explore the weather
+    </v-btn>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.highlight-text {
+  font-weight: 600;
+  background: linear-gradient(
+      to right,
+      rgb(0, 193, 124),
+      rgb(0, 178, 178),
+      rgb(0, 117, 209),
+      rgb(0, 193, 124),
+  );
+  background-clip: text;
+  color: transparent;
+  background-size: 200%;
+  animation: gradientAnimation 1.5s linear infinite;
+}
+
+@keyframes gradientAnimation {
+  0% {
+    background-position: 200% 50%;
+  }
+
+  100% {
+    background-position: 0 50%;
+  }
+}
+
+</style>
