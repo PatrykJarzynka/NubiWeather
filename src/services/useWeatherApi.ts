48bb9ef9e3cb3b99b52379a132e1cdf296dff28f
Patryk Jarzynka <patryk.jarzynka@codeconcept.pl>
2025-07-25 11:46:21 +0200

    Updated api url

--- src/services/useWeatherApi.ts
+++ src/services/useWeatherApi.ts
@@ -3,7 +3,7 @@ import { ForecastWeatherResponse } from '@/interfaces/ForecastWeatherResponse.ts
 
 export function useWeatherApi() {
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-  const BASE_URL = 'http://api.weatherapi.com/v1';
+  const BASE_URL = 'https://api.weatherapi.com/v1';
 
   const { data, error, isLoading, execute } = useFetch<ForecastWeatherResponse>()
 
